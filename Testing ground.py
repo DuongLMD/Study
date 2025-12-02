b41ef725d9290497b5095f94908c4e26a2a6081e
Le Minh Duong <duonglm.ptit@gmail.com>
2025-12-02 16:42:00 +0700

    2.1 Bien trong python
    for study purposess

---
+++ Testing ground.py
@@ -0,0 +1,22 @@
+#2.1 BIẾN TRONG PYTHON
+x = 5
+y = "John"
+print (x)
+print (y)
+#
+x = 5
+print (id(x))
+#địa chỉ bộ nhớ của biến x
+x = 4
+x = "Duo"
+print (x)
+#Gán giá trị cho biến mới nhất
+x = 5
+print(x)
+del x
+#Xoá biến x khỏi bộ nhớ , gây lỗi khi in x (print (x))
+x = 5 
+y = "John"
+print(type(x)) #số là kiểu int
+print(type(y)) #chữ là kiểu str
+#Lấy kiểu biến
